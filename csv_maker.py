9de1fe362ada7126523b150ca38c56da30864d0a
Jenny Liang <jingjing.liang@bc.edu>
2019-11-26 13:31:44 -0500

    Update csv_maker.py
    added the levenshtein distance

--- csv_maker.py
+++ csv_maker.py
@@ -25,7 +25,6 @@ mp4List = [file for file in glob.glob('**/*.mp4', recursive=True)]
 wavList = [file.replace('.mp4', '.wav') for file in mp4List]
 txtList = [file.replace('.mp4', '.txt') for file in mp4List]
 
-
 def getVideo(index):
     filename = mp4List[index]
     vdata = skvideo.io.vread(name, as_grey = True)
@@ -36,6 +35,7 @@ def getVideo(index):
     diff_tensor = torch.tensor(diff_array)
     final = torch.cat([vtensor, diff_tensor])
     return final
+
 '''
  # T = number of frames
  # M = height
@@ -86,3 +86,35 @@ To check
 '''
 test = torch.load('video.pt')
 print(test.shape)
+
+
+
+'''
+Calculate the error
+'''
+
+def levenshtein(seq1, seq2):
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros ((size_x, size_y))
+    for x in range(size_x):
+        matrix [x, 0] = x
+    for y in range(size_y):
+        matrix [0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x-1] == seq2[y-1]:
+                matrix [x,y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1],
+                    matrix[x, y-1] + 1
+                )
+            else:
+                matrix [x,y] = min(
+                    matrix[x-1,y] + 1,
+                    matrix[x-1,y-1] + 1,
+                    matrix[x,y-1] + 1
+                )
+    #print (matrix)
+    return (matrix[size_x - 1, size_y - 1])
